cb3ab39c1e5f9d28d968330922de92a0fe983e47
Andrey Sumin <an.sumin@hh.ru>
2014-09-17 21:48:19 +0400

    benchmark

--- instances.py
+++ instances.py
@@ -10,7 +10,7 @@ import requests
 from lxml import etree
 
 import tornado.options
-import tornado_util.supervisor as supervisor
+from tornado_util import supervisor
 
 try:
     import sys
@@ -20,48 +20,52 @@ except ImportError:
     USE_COVERAGE = False
 
 
+def wait_for(fun, n=100):
+    for i in xrange(n):
+        if fun():
+            return
+        time.sleep(0.01)
+    assert fun()
+
+
+def get_free_port():
+    for port in xrange(9000, 10000):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.bind(('', port))
+            s.close()
+            break
+        except:
+            pass
+    else:
+        raise AssertionError('No empty port in range 9000..10000 for frontik test instance')
+
+    return port
+
+
 class FrontikTestInstance(object):
     def __init__(self, app, config):
         tornado.options.parse_config_file(config)
         self.app = app
         self.config = config
         self.port = None
-        self.supervisor = supervisor
 
     def start(self):
-        for port in xrange(9000, 10000):
-            try:
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.bind(('', port))
-                s.close()
-                break
-            except:
-                pass
-        else:
-            raise AssertionError('No empty port in range 9000..10000 for frontik test instance')
+        self.port = get_free_port()
 
         if USE_COVERAGE:
             script = 'coverage run -p --branch --source=frontik ./frontik-test'
         else:
             script = './frontik-test'
 
-        supervisor.start_worker(script, app=self.app, config=self.config, port=port)
-        self.wait_for(lambda: supervisor.is_running(port))
-        self.port = port
+        supervisor.start_worker(script, app=self.app, config=self.config, port=self.port)
+        wait_for(lambda: supervisor.is_running(self.port))
 
     def stop(self):
         if self.port is not None:
-            self.supervisor.stop_worker(self.port)
-            self.wait_for(lambda: not(self.supervisor.is_running(self.port)))
-            self.supervisor.rm_pidfile(self.port)
-
-    @staticmethod
-    def wait_for(fun, n=100):
-        for i in xrange(n):
-            if fun():
-                return
-            time.sleep(0.01)
-        assert fun()
+            supervisor.stop_worker(self.port)
+            wait_for(lambda: not(supervisor.is_running(self.port)))
+            supervisor.rm_pidfile(self.port)
 
     def get_page(self, page, notpl=False, **kwargs):
         if not self.port:
